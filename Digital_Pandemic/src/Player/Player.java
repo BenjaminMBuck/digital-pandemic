379ce80b63fc60553a5710f4c9514c4c3129e674
Benjamin Buck <bbuck@clemson.edu>
2018-10-13 10:32:44 -0400

    Add some attributes to the Player class

--- Digital_Pandemic/src/Player/Player.java
+++ Digital_Pandemic/src/Player/Player.java
@@ -1,5 +1,12 @@
 package Player;
 
-public class Player {
+import java.util.ArrayList;
 
+import Cards.PlayerCard;
+import Cards.RoleCard;
+
+public class Player {
+	RoleCard role;
+	ArrayList<PlayerCard> hand;
+	int playerNumber;
 }
