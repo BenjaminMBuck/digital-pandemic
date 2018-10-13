11497b28739e9f03fedf846750a17447a1cea6e5
Benjamin Buck <bbuck@clemson.edu>
2018-10-13 11:35:26 -0400

    Add card shuffling

--- Digital_Pandemic/src/Cards/Card.java
+++ Digital_Pandemic/src/Cards/Card.java
@@ -1,9 +1,23 @@
 package Cards;
 
+import java.util.ArrayList;
+
 public class Card {
 
   public Card(){
   
+  }
+  
+  public static ArrayList<Card> shuffleCardDeck(ArrayList<Card> deck) {
+	  ArrayList<Card> shuffledDeck = new ArrayList<Card>(deck);
+	   for(int i = 0; i < deck.size(); i++) {
+		   int rand = (int) (Math.random() * (deck.size() - 1));
+		   Card randomCard = deck.get(rand);
+		   deck.remove(rand);
+		   shuffledDeck.add(randomCard);
+	   }
+	  
+	  return shuffledDeck;
   }
   
 }
