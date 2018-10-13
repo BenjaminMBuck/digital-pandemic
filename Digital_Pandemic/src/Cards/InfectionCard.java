b05764b6bc28f57067eb50e8df00282231f6f702
SHUF4 <44116382+SHUF4@users.noreply.github.com>
2018-10-13 12:05:43 -0400

    Update InfectionCard.java

--- Digital_Pandemic/src/Cards/InfectionCard.java
+++ Digital_Pandemic/src/Cards/InfectionCard.java
@@ -1,10 +1,7 @@
 package Cards;
 
 public class InfectionCard extends Card{
-  private String cityList["Beijing","Shanghai","Hong Kong","Bangkok","Jakarta","Ho Chi Minh City","Seoul,Osaka","Taipei","Manila","Sydney","Tokyo",
-"Los Angeles","Mexico City","Miami","Bogota","Lima,Santiago","Buenos Aires","Sao Paulo","Lagos,Kinshasa","JohannesBurg","Khartoum","San Francisco","Chicago",
-"Washinton","New York","Montreal","London","Madrid","Paris","Milan","St.Peterson","Essen","Atlanta","Moscow","Tehran","Delhi","Kolkata",
-"Chennai","Mumbai","Karaachi","Riyadh","Cairo","Istanbul","Baghdad","Algiers"];
+  
   
   
   
