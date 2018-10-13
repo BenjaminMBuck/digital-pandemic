4a7d0a72c5c97e7fdb6f679a3660a5a00564ff18
Benjamin Buck <bbuck@clemson.edu>
2018-10-13 11:05:17 -0400

    Create the initial prompt for player and epidemic numbers

--- Digital_Pandemic/src/GUI/InitialPrompt.java
+++ Digital_Pandemic/src/GUI/InitialPrompt.java
@@ -1,12 +1,28 @@
 package GUI;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class InitialPrompt
 {
-    private JFrame frame;
+    private JDialog dialog;
     JPanel panel;
+    
+    JRadioButton twoPlayer;
+    JRadioButton threePlayer;
+    JRadioButton fourPlayer;
+    
+    JRadioButton fourEpidemic;
+    JRadioButton fiveEpidemic;
+    JRadioButton sixEpidemic;
 
     private String title;
     private int width, height;
@@ -23,23 +39,96 @@ public class InitialPrompt
 
     private void createDisplay()
     {
-        frame = new JFrame(title);
-        frame.setSize(width, height);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
+    	JFrame f = new JFrame();
+        dialog = new JDialog(f, "Pandemic", true);
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setSize(width, height);
+        dialog.setResizable(false);
+        dialog.setLocationRelativeTo(null);
         
         panel = new JPanel();
         
         JLabel numPlayerPromptText = new JLabel("Choose the number of players.");
         panel.add(numPlayerPromptText);
         
+        ButtonGroup numPlayers = new ButtonGroup();
+        
+        twoPlayer = new JRadioButton("Two Players");
+        threePlayer = new JRadioButton("Three Players");
+        fourPlayer = new JRadioButton("Four Players");
+        
+        numPlayers.add(twoPlayer);
+        numPlayers.add(threePlayer);
+        numPlayers.add(fourPlayer);
+        
+        twoPlayer.setSelected(true);
+        
+        panel.add(twoPlayer);
+        panel.add(threePlayer);
+        panel.add(fourPlayer);
+        
+        
+        
         JLabel numEpidemicsPromptText = new JLabel("Choose the number of epidemics.");
         panel.add(numEpidemicsPromptText);
         
         
-        frame.getContentPane().add(panel);
-        frame.setVisible(true);
+        ButtonGroup numEpidemic = new ButtonGroup();
+        
+        fourEpidemic = new JRadioButton("Four Epidemics");
+        fiveEpidemic = new JRadioButton("Five Epidemics");
+        sixEpidemic = new JRadioButton("Six Epidemics");
+        
+        numEpidemic.add(fourEpidemic);
+        numEpidemic.add(fiveEpidemic);
+        numEpidemic.add(sixEpidemic);
+        
+        fourEpidemic.setSelected(true);
+        
+        panel.add(fourEpidemic);
+        panel.add(fiveEpidemic);
+        panel.add(sixEpidemic);
+        
+        JButton enterButton = new JButton("Enter");
+        enterButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				dialog.dispatchEvent(new WindowEvent(dialog, WindowEvent.WINDOW_CLOSING));
+			}
+        });
+        
+        panel.add(enterButton);
+        
+        
+        dialog.getContentPane().add(panel);
+        dialog.setVisible(true);
 
+    }
+    public int getNumPlayers() {
+    	int numPlayers = 2;
+    	if (twoPlayer.isSelected()){
+    		numPlayers = 2;
+    	}
+    	if (threePlayer.isSelected()) {
+    		numPlayers = 3;
+    	}
+    	if (fourPlayer.isSelected()) {
+    		numPlayers = 4;
+    	}
+    	return numPlayers;
+    }
+    public int getNumEpidemic() {
+    	int numEpidemic = 4;
+    	if (fourEpidemic.isSelected()){
+    		numEpidemic = 4;
+    	}
+    	if (fiveEpidemic.isSelected()) {
+    		numEpidemic = 5;
+    	}
+    	if (sixEpidemic.isSelected()) {
+    		numEpidemic = 6;
+    	}
+    	return numEpidemic;
     }
 }
--- Digital_Pandemic/src/main/Main.java
+++ Digital_Pandemic/src/main/Main.java
@@ -1,8 +1,24 @@
 package main;
 
+import java.util.ArrayList;
+
+import Cards.PlayerCard;
+import GUI.InitialPrompt;
+
 public class Main {
-	public static void main() {
-		//hi
+	public static int numPlayers;
+	public static int numEpidemics;
+	
+	public static ArrayList<InfectionCard> infectionDrawPile;
+	public static ArrayList<InfectionCard> infectionDiscardPile;
+	public static ArrayList<PlayerCard> playerDrawPile;
+	public static ArrayList<PlayerCard> playerDiscardPile;
+	
+	
+	public static void main(String args[]) {
+		InitialPrompt window = new InitialPrompt();
+		numPlayers = window.getNumPlayers();
+		numEpidemics = window.getNumEpidemic();
 		
 	}
 }
