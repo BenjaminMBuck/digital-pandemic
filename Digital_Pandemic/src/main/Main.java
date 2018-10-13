665c35f2833b66badf55d535285078c77656f7aa
ClemsonJames <44116508+ClemsonJames@users.noreply.github.com>
2018-10-13 13:41:28 -0400

    Add files via upload

--- Digital_Pandemic/src/main/Main.java
+++ Digital_Pandemic/src/main/Main.java
@@ -20,5 +20,31 @@ public class Main {
 		numPlayers = window.getNumPlayers();
 		numEpidemics = window.getNumEpidemic();
 		
+		//Red Cities
+		City Beijing = new City("Beijing", 2,1,1,false);
+		City Shanghai = new City("Shanghai", 2,1,1,false);
+		City hongKong = new City("Hong Kong", 2,1,1,false);
+		City Bangkok = new City("Bangkok", 2,1,1,false);
+		City Jakarta = new City("Jakarta", 2,1,1,false);
+		City HCMCity = new City("Ho Chi Minh City", 2,1,1,false);
+		City Seoul = new City("Seoul", 2,1,1,false);
+		City Osaka = new City("Osaka", 2,1,1,false);
+		City Taipei = new City("Taipei", 2,1,1,false);
+		City Manila = new City("Manila", 2,1,1,false);
+		City Sydney = new City("Sydney", 2,1,1,false);
+		City Tokyo = new City("Tokyo", 2,1,1,false);
+		//Yellow Cities
+		City LosAngeles = new City("Beijing", 1,1,1,false);
+		City MexicoCity = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
+		City  = new City("Beijing", 1,1,1,false);
 	}
 }
