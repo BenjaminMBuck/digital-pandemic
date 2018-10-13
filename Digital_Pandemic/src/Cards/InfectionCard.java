95e61894a97db3dd930cc4f89ced32c9678f90b8
SHUF4 <44116382+SHUF4@users.noreply.github.com>
2018-10-13 11:12:22 -0400

    Update InfectionCard.java

--- Digital_Pandemic/src/Cards/InfectionCard.java
+++ Digital_Pandemic/src/Cards/InfectionCard.java
@@ -1,9 +1,11 @@
 package Cards;
 
 public class InfectionCard extends Card{
-  private int cardColor;
+  private String cityList[Beijing,Shanghai,Hong Kong,Bangkok,Jakarta,Ho Chi Minh City,Seoul,Osaka,Taipei,Manila,Sydney];
   public InfectionCard(int color){
     this.cardColor=color;
   }
+  
+  
 
 }
